0b56bc74f82f394cc6321a3acec0d8c4cd14a6d6
Amandine.Cimmino <130897448+A6miNo@users.noreply.github.com>
2023-11-13 18:57:23 +0100

    mise a jour style index

--- js/script.js
+++ js/script.js
@@ -21,3 +21,45 @@ $(document).ready(function () {
       $('#heroCarousel').carousel('next');
     });
   });
+
+  //Animation
+  $(document).ready(function() {
+    //  Animation image qui fait une rotation
+    $("#img-anim").animate({ opacity: 1, deg: 360 }, {
+      duration: 2000, 
+      step: function(now) {
+        $(this).css({ transform: 'rotate(' + now + 'deg)' });
+      }
+    });
+
+  });
+
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      // Récupérer la position de l'élément par rapport au haut de la fenêtre
+     const scrollPosition = $(this).scrollTop();
+      
+      // Récupérer la position de l'élément
+      const elementPosition = $('.fadeInOnScroll').offset().top;
+      
+      // Calculer la distance entre l'élément et la position actuelle du scroll
+      const distance = (elementPosition - scrollPosition);
+      
+      // Si l'élément est dans la vue (dans la fenêtre de vision)
+      if (distance < $(window).height() - 50) {
+        // Faire un fadeIn en réglant l'opacité sur 1
+        $('.fadeInOnScroll').fadeTo(1000, 1);
+      }
+    });
+  });
+
+
+  //animation filtre
+  $(document).ready(function() {
+    $('categories btn').click(function(){
+        let category=$(this).attr('data-category');
+        $('actu').hide();
+        $('.'+category).show()
+
+    })
+  })
