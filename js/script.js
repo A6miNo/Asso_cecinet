2fc880cf992096d4e671ce711d2d243d6ee50918
Amandine.Cimmino <130897448+A6miNo@users.noreply.github.com>
2023-11-13 16:40:20 +0100

    modification index dont slide

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
 document.getElementById('backtotop').addEventListener('click', function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+//Slide
+$(document).ready(function () {
+    // Activer le carrousel avec les options spécifiées
+    $('#heroCarousel').carousel({
+      interval: 3000, // Changement automatique
+      pause: 'hover' // Pause lorsque la souris survole le carrousel
+    });
+
+    // Navigation manuelle avec les flèches
+    $('.carousel-control-prev').click(function () {
+      $('#heroCarousel').carousel('prev');
+    });
+
+    $('.carousel-control-next').click(function () {
+      $('#heroCarousel').carousel('next');
+    });
+  });
